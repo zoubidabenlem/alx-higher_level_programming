9465d4e3260f5e64cd83247b9fd4467bbdbeb025
Benlem <zoubidabenlem@gmail.com>
2024-03-16 23:57:48 +0000

    so slow

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i+1, 10):
-        if(i == 8):
-            print("{}{}".format(i,j))
+        if (i == 8):
+            print("{}{}".format(i, j))
             break
-        print("{}{}".format(i,j), end=", ")
-print("\n")
+        print("{}{}".format(i, j), end=", ")
