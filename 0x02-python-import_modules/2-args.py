833be08f238db9aa33a7af60c8f6e5e5af36b121
Benlem <zoubidabenlem@gmail.com>
2024-03-17 14:18:48 +0000

    cih

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    size = len(sys.argv) - 1
+    if size == 0:
+        print("{} arguments.".format(size))
+        sys.exit()
+    print("{} arguments:".format(size))
+    for i in range(1, size + 1):
+        print("{}: {}".format(i, sys.argv[i]))
