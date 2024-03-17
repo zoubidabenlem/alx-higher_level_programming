de4849447047dd1caf89523fe7a58674f8b2748f
Benlem <zoubidabenlem@gmail.com>
2024-03-17 01:29:22 +0000

    world

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 result = ""
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        result+=chr(i)
+        result += chr(i)
     else:
-        result+=chr(i-32)
-print("{}".format(result),end="")
+        result += chr(i-32)
+print("{}".format(result), end="")
