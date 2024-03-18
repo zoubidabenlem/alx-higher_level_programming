ec4a6e77123b7e6227a853e168f35948c0818011
Benlem <zoubidabenlem@gmail.com>
2024-03-18 14:43:33 +0000

    added dumbass equal

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list == []:
-        sys.exit(1)
+        pass
     else:
         for item in reversed(my_list):
             print("{:d}".format(item))
