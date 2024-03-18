fae20b99af7b045f19cfa0725870df186ef367e4
Benlem <zoubidabenlem@gmail.com>
2024-03-18 14:40:39 +0000

    cond add

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for item in reversed(my_list):
-        print("{:d}".format(item))
+    if my_list = []:
+        sys.exit(1)
+    else:
+        for item in reversed(my_list):
+            print("{:d}".format(item))
