688669ab35b3d467441d0f107143f96c0532f6ec
Benlem <zoubidabenlem@gmail.com>
2024-03-20 21:50:23 +0000

    frick commits

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        sys.exit(1)
-    return max({value for key, value in a_dictionary.items()})
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
