88efe47124cf322dde9c512f81a1a130ddec38c3
Benlem <zoubidabenlem@gmail.com>
2024-03-20 18:45:06 +0000

    pycodestyle fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newList = [replace if ele == search else ele for ele in my_list ]
+    newList = [replace if ele == search else ele for ele in my_list]
     return newList
