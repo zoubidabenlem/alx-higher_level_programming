305cc503096071fbb11e238adafc996f6ef7ef9d
Benlem <zoubidabenlem@gmail.com>
2024-03-20 22:10:48 +0000

    frick commits

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/bin/usr/python3
 def multiply_list_map(my_list=[], number=0):
-    list_mul = list(map(lambda v : v * number, my_list))
-    return list_mul
+    return list(map(lambda v : v * number, my_list))
