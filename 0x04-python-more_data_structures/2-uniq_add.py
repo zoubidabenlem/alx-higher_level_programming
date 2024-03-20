db23eaab20e15f4fbf3deb8151b918d8bfab4a7b
Benlem <zoubidabenlem@gmail.com>
2024-03-20 19:28:25 +0000

    frick commits

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,3 @@
-def uniq_add(my_list=[]):
-    sum = 0
-    for i in set(my_list):
-        sum += i
-    return sum
+#!/usr/bin/python3
+def uniq_add(my_list=[])
+    return sum(ele for ele in set(my_list))
