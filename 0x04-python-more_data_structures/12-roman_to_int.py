ae98b6f58d8018b77dd725270a514c51df1cdcac
Benlem <zoubidabenlem@gmail.com>
2024-03-21 02:38:07 +0000

    frick commits

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,4 +15,4 @@ def roman_to_int(roman_string):
                     prev = True
                     if letter == 'I':
                         prevLet = True
-    return number
+    return int(number)
