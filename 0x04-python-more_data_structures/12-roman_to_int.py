fa32b975932be4e27e339967ff9d3776c7deb544
Benlem <zoubidabenlem@gmail.com>
2024-03-21 02:28:03 +0000

    frick commits

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,21 @@
 #!/bin/usr/python3
 def roman_to_int(roman_string):
-    romanNum = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    rN = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number = 0
-    global prev 
     prev = False
     prevLet = False
+
     for letter in (reversed(roman_string)):
-        for k, v in romanNum.items():
-            if letter == k :
-                if letter == 'I' and prev and not prevLet  :
+        for k, v in rN.items():
+            if letter == k:
+                if letter == 'I' and prev and not prevLet:
                     number -= 1
                 else:
                     number += v
                     prev = True
                     if letter == 'I':
-                            prevLet = True
+                        prevLet = True
     return number
-
