42cfdebb7dc2a8a35d51292c188deb982e2b5f65
Benlem <zoubidabenlem@gmail.com>
2024-04-02 21:43:53 +0000

    fixed loop and exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-    except (IndexError, ValueError, TypeError):
-        pass
-    finally:
-        print()
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            raise
+    print()
     return count
