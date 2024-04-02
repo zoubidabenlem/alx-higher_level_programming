29018e16cb0355ec7d7ec4922206495aea4346ec
Benlem <zoubidabenlem@gmail.com>
2024-04-02 22:19:10 +0000

    init com

--- 0x05-python-exceptions/main-tests.py
+++ 0x05-python-exceptions/main-tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-#Test 0
+##################################################################Test 0
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
@@ -14,7 +14,7 @@ nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
 
 
-#Test 1
+####################################################################Test 1
 safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
 value = 89
@@ -33,7 +33,7 @@ if not has_been_print:
     print("{} is not an integer".format(value))
 
 
-#Test 3
+###################################################################Test 3
 safe_print_list_integers = \
     __import__('2-safe_print_list_integers').safe_print_list_integers
 
@@ -48,9 +48,9 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-'''
 
-#Test 4
+
+####################################################################Test 4
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 a = 12
@@ -62,3 +62,27 @@ a = 12
 b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
+
+
+####################################################################Test 5
+list_division = __import__('4-list_division').list_division
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+'''
+####################################################################Test 6
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
