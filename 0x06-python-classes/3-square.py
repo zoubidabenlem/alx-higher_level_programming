6b634fb24fdb0ecca51705044be020ba67434168
Benlem <zoubidabenlem@gmail.com>
2024-04-03 01:27:53 +0000

    init com

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''attribute definition and error handling'''
+
+
+class Square:
+    '''square but with size and type exceptions'''
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    '''calculate square'''
+    def area(self):
+        return self.__size ** 2
--- 0x06-python-classes/main-tests.py
+++ 0x06-python-classes/main-tests.py
@@ -25,10 +25,9 @@ try:
     print(my_square.__size)
 except Exception as e:
     print(e)
-'''
+
 
 ######################################### TEST 2
-#!/usr/bin/python3
 Square = __import__('2-square').Square
 
 my_square_1 = Square(3)
@@ -62,3 +61,21 @@ try:
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
+'''
+Square = __import__('3-square').Square
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
