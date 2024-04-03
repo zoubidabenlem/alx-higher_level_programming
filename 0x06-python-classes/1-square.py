7133c9651c5dd6df7e00cadb0cbb1b3bc78ccdcb
Benlem <zoubidabenlem@gmail.com>
2024-04-03 00:11:07 +0000

    init commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''attribute definition'''
+
+
+class Square:
+    '''square but with size'''
+    def __init__(self, size):
+        self.__size = size
--- 0x06-python-classes/main-tests.py
+++ 0x06-python-classes/main-tests.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 
+'''
 ########################################## TEST 0
 Square = __import__('0-square').Square
 
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
+'''
 
+######################################### TEST 1
+Square = __import__('1-square').Square
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
