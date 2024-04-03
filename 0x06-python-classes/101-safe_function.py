1cf2eaa0dadf3d44e28cb91282222bdff23855cb
Benlem <zoubidabenlem@gmail.com>
2024-04-03 02:38:52 +0000

    101 100 push

--- 0x06-python-classes/101-safe_function.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def safe_function(fct, *args):
-    from sys import stderr
-    try:
-        return fct(*args)
-    except Exception as msg:
-        print("Exception: {}".format(msg), file=stderr)
