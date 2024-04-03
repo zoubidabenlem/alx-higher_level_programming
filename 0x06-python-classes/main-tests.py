5610c97a27ca139a6c67c043f88d37749656e119
Benlem <zoubidabenlem@gmail.com>
2024-04-03 00:21:26 +0000

    initial commit

--- 0x06-python-classes/main-tests.py
+++ 0x06-python-classes/main-tests.py
@@ -7,7 +7,7 @@ Square = __import__('0-square').Square
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
-'''
+
 
 ######################################### TEST 1
 Square = __import__('1-square').Square
@@ -23,5 +23,42 @@ except Exception as e:
 
 try:
     print(my_square.__size)
+except Exception as e:
+    print(e)
+'''
+
+######################################### TEST 2
+#!/usr/bin/python3
+Square = __import__('2-square').Square
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
 except Exception as e:
     print(e)
