9482daf9233dc48e9e38de20c01cedf6ccff7654
Benlem <zoubidabenlem@gmail.com>
2024-04-17 21:55:55 +0100

    task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,6 +8,6 @@ def is_same_class(obj, a_class):
     an instance of a_class, otherwise
     it returns False
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
